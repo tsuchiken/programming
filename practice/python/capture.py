d7cc27e71d8f1934daa720aadb00b8d3c8651066
Kengo T <kenkenwxp@gmail.com>
2018-05-05 20:30:38 +0900

    change Comment of imageCapture.py & copy to capture.py

--- practice/python/capture.py
+++ practice/python/capture.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-import tkinter
+from tkinter import *
+from datetime import *
 import cv2
+import sys
+import os
 
 #Define capture device
 defaultDevice = 0
@@ -12,24 +15,37 @@ vc = cv2.VideoCapture(defaultDevice + apiID)
 #Deal for the device open error
 if not vc.isOpened():
 	raise DeviceOpenException("Device open is failed")
-	
+
+#Set properties
 vc.set(cv2.CAP_PROP_FPS,60)
-while True:
+
+while vc.isOpened():
 	#Read images in every frame
 	ret, frame = vc.read()
 
 	#Desplay images
 	cv2.imshow("test",frame)
-	filename = "test"+str(cv2.CAP_PROP_FRAME_COUNT)+".png"
+	"""
+	CheckMethod
 	print(filename,str(round(vc.get(cv2.CAP_PROP_FPS),0)))
+	"""
 
 	#Wait a key in every 1msec
 	key = cv2.waitKey(1)
 
 	#if s key is pushed, save the image of this frame
 	if key == ord("s"):
-		cv2.imwrite("D:\\Pictures\\test.png",frame)
-	#if ESC key is pushed, break though this loop
+		path = "D:\\Pictures\\"
+		os.makedirs(path,0o777,True)
+		fnTime = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+		"""
+		CheckDeal
+		print(fnTime)
+		"""
+		filename = "cap_" + fnTime + ".png"
+		cv2.imwrite(path + filename,frame)
+	#if ESC key is pushed, the system break though this loop
 	elif key == 27:
 		break
 
